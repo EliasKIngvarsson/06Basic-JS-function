2b526103e713604984a4fe8e549af8da7db6d5c3
EliasKIngvarsson <iljakarevski@gmail.com>
2022-03-27 20:56:28 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var a
+var b
+var diff
+function calcDifference(a,b){
+ var diff= a-b
+console.log(diff);
+}
+
+
+var string
+function myNameIs(string){
+console.log("My name is " + string);
+}
+myNameIs("Gunnsteinn")
+
+var k
+var s
+function calcModulus(k,s){
+var modulus= k%s
+console.log(modulus);
+}
+calcModulus();
+
+
+var t
+function isEven(t){
+        if (t%2 ==0){
+            console.log("True")
+        }
+        else {
+            console.log("False")
+        }
+}
+isEven(1)
